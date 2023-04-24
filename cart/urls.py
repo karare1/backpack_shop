ed8d0cebd82673f9423b2f9e6ac2f767dcc9a3ca
karare1 <defi20@yahoo.co.uk>
2023-04-24 00:21:58 +0000

    Add view and url for add items to cart

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shop_cart, name='shop_cart')
+    path('', views.shop_cart, name='shop_cart'),
+    path('add/<item_id>/', views.add_items, name='add_items'),
 ]
