19f307d1f1095c183fb92acf9cf997d886b6384d
karare1 <defi20@yahoo.co.uk>
2023-04-29 23:49:38 +0000

    Add views and urls for update and remove items from cart

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.shop_cart, name='shop_cart'),
     path('add/<item_id>/', views.add_items, name='add_items'),
+    path('amend/<item_id>/', views.amend_item, name='amend_item'),
+    path('remove/<item_id>/', views.remove_item, name='remove_item'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -24,3 +24,53 @@ def add_items(request, item_id):
     request.session['cart'] = cart
     # print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def amend_item(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    # size = None
+    # if 'product_size' in request.POST:
+    #     size = request.POST['product_size']
+    cart = request.session.get('cart', {})
+
+    # if size:
+    #     if quantity > 0:
+    #         bag[item_id]['items_by_size'][size] = quantity
+    #     else:
+    #         del bag[item_id]['items_by_size'][size]
+    #         if not bag[item_id]['items_by_size']:
+    #             bag.pop(item_id)
+    # else:
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('shop_cart'))
+
+
+def remove_item(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        # size = None
+        # if 'product_size' in request.POST:
+        #     size = request.POST['product_size']
+        cart = request.session.get('cart', {})
+
+        # if size:
+        #     del bag[item_id]['items_by_size'][size]
+        #     if not bag[item_id]['items_by_size']:
+        #         bag.pop(item_id)
+        # else:
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
