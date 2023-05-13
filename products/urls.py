8c7f8d920d4582c9273722eb31a0dbbee845dd4e
karare1 <defi20@yahoo.co.uk>
2023-05-13 12:40:50 +0000

    Add url, view and template for editing products

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.individual_product, name='individual_product'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
