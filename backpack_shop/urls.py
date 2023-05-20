54413a1adb11579c9d478252d6593280b4f2fcfc
karare1 <defi20@yahoo.co.uk>
2023-05-20 00:25:54 +0000

    Add urls for contact and my_wishlist

--- backpack_shop/urls.py
+++ backpack_shop/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('my_profile/', include('profiles.urls')),
+    path('contact/', include('contact.urls')),
+    path('my_wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
