3a0bae1e0daf9cd0c1460181c4700e68edefc4f1
karare1 <defi20@yahoo.co.uk>
2023-05-20 00:42:31 +0000

    Add validation to product quantity

--- static/js/script.js
+++ static/js/script.js
@@ -44,7 +44,7 @@ $('.decrement-qty').click(function(e) {
  // Update quantity on click
  $('.update-link').click(function(e) {
     var form = $(this).prev('.update-form');
-    form.submit();
+    form[0].requestSubmit();
 })
 
 // Remove item and reload on click
