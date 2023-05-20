c42ae1460b5209ac1f7b974d52e2012fdce63161
karare1 <defi20@yahoo.co.uk>
2023-05-20 00:39:44 +0000

    Add ReviewForm

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Product, Category
+from .models import Review
 
 from .widgets import CustomClearableFileInput
 
@@ -20,3 +21,9 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('user', 'review', 'rating',)
